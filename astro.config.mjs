e398023b13ff7165c09a0041bb4415008ee7677c
Isaac Spencer <22ispencer@gmail.com>
2024-08-01 18:08:03 -0500

    added htmx, alpinejs, tailwind, prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import htmx from "astro-htmx";
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), alpinejs(), htmx()],
+});
