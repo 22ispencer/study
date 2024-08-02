e50a69f23e74c50355ba505a2f3c7cd3c7ee8299
Isaac Spencer <22ispencer@gmail.com>
2024-08-01 22:36:54 -0500

    clerk makes auth so easy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import htmx from "astro-htmx";
 import alpinejs from "@astrojs/alpinejs";
-
 import db from "@astrojs/db";
+import node from "@astrojs/node";
+import clerk from "@clerk/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs(), htmx(), db()]
+  integrations: [tailwind(), alpinejs(), htmx(), db(), clerk()],
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
